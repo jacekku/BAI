1385e90d952dc6a7e5d3259b9a70519a4ddd8dbd
kaloria10 <mszycakarol@gmail.com>
2021-04-19 21:38:43 +0200

    add orders view

---
+++ src/components/Orders.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <Navbar></Navbar>
+    <div class="main">
+      <div class="container w-50">
+     <b-row>
+            <b-col>
+              <b-table
+                striped
+                hover
+                :items="orders" 
+              >
+                
+              </b-table>
+            </b-col>
+          </b-row>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'Orders',
+  components: {
+    Navbar: Navbar,
+  },
+  data() {
+    return {
+        orders: [],
+    }
+  },
+  mounted () {
+    fetch('https://my.api.mockaroo.com/orders.json?key=646a4130')
+            .then((fetchedData) => fetchedData.json())
+            .then(
+            (fetchedData) => (this.orders = fetchedData)
+            )
+            .catch((err) => console.log(err.message))
+  },
+}
+</script>
